6c2d0a3064b59019e2098d4bd645ce1abc123707
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-12-30 22:33:55 +0600

    Task1

---
+++ Pelmeni.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pelmen
+{
+    abstract class Pelmeni
+    {
+        public void Make()
+        {
+            getName();
+            addMeet();
+            AddDough();
+            AddSpice();
+            AddSalt();
+        }
+        public abstract string Name { get; }
+        public abstract void getName();
+        public abstract void addMeet();
+        public abstract void AddDough();
+        public abstract void AddSpice();
+        public abstract void AddSalt();
+        public virtual void getReadyEat()
+        {
+            Console.WriteLine("Получили еду");
+        }
+        class Cupecheskie : Pelmeni
+        {
+            public override string Name
+            {
+                get
+                {
+                    return "Пельмени Купеческие на развес";
+                }
+            }
+
+            public override void addMeet()
+            {
+                Console.WriteLine("Добавляем мясо");
+            }
+
+            public override void AddDough()
+            {
+                Console.WriteLine("Добавляем тесто");
+            }
+
+            public override void AddSalt()
+            {
+                Console.WriteLine("Добавляем добаляем соль в мясо");
+            }
+
+            public override void AddSpice()
+            {
+                Console.WriteLine("Добавляем специи");
+            }
+
+            public override void getName()
+            {
+                Console.WriteLine("Добавляем" + this.Name);
+            }
+        }
+        class Bavarskie : Pelmeni
+        {
+            public override string Name
+            {
+                get
+                {
+                    return "пельмени Баварские на развес";
+                }
+            }
+
+            public override void addMeet()
+            {
+                Console.WriteLine("Добавляем мясо");
+            }
+
+            public override void AddDough()
+            {
+                Console.WriteLine("Добавляем тесто");
+            }
+
+            public override void AddSalt()
+            {
+                Console.WriteLine("Добавляем добаляем соль в мясо");
+            }
+
+            public override void AddSpice()
+            {
+                Console.WriteLine("Добавляем специи");
+            }
+
+            public override void getName()
+            {
+                Console.WriteLine("Добавляем" + this.Name);
+            }
+        }
+        class Studencheskie : Pelmeni
+        {
+            public override string Name
+            {
+                get
+                {
+                    return "Пельмени Студенческие на развес";
+                }
+            }
+
+            public override void addMeet()
+            {
+                Console.WriteLine("Добавляем мясо");
+            }
+
+            public override void AddDough()
+            {
+                Console.WriteLine("Добавляем тесто");
+            }
+
+            public override void AddSalt()
+            {
+                Console.WriteLine("Добавляем добаляем соль в мясо");
+            }
+
+            public override void AddSpice()
+            {
+                Console.WriteLine("Добавляем специи");
+            }
+
+            public override void getName()
+            {
+                Console.WriteLine("Добавляем" + this.Name);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Pelmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelmeni someCoffeeDrink = new Bavarskie();
+            Cupecheskie espresso = new Cupecheskie();
+            someCoffeeDrink.Make();
+            espresso.Make();
+        }
+    }
+}
